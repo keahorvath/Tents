1e1bf4fd4f9ec337262b88fea8697e2500946d6e
Kea Horvath <kea.horvath@etu.u-bordeaux.fr>
2020-10-22 18:07:49 +0200

    Ajout du projet game_text avec CMake

--- game_aux.c
+++ game_aux.c
@@ -4,41 +4,18 @@
 #include "game.h"
 
 
-
-/**
- * @brief Prints a game as text on the standard output stream.
- * @details The different squares EMPTY, TREE, TENT, and GRASS are respectively
- * displayed with the following characters ' ', 'x', '*', '-'.
- * Here is the attempted output for the default game:
-@verbatim
-   01234567
-   --------
-0 |    xx  | 3
-1 |x      x| 0
-2 |    x   | 4
-3 |x    x  | 0
-4 | x      | 4
-5 |x   x x | 0
-6 |        | 1
-7 |x       | 0
-   --------
-   40121121
-@endverbatim
- * @param g the game
- * @pre @p g must be a valid pointer toward a game structure.
- **/
-
 void game_print(cgame g){
 	if (g == NULL){
 		fprintf(stderr, "function called on null address");
 		exit(EXIT_FAILURE);
 	}
 	printf("   ");
-	for (uint i = 1; i < 8; i++){
+	for (uint i = 0; i < 8; i++){
 		printf("%u", i);
 	}
+	printf("   ");
 	printf("\n");
-	printf("   --------");
+	printf("   --------   \n");
 	for (uint i = 0; i < 8; i++){
 		printf("%u |", i);
 		for (uint j = 0; j < 8; j++){
@@ -57,19 +34,18 @@ void game_print(cgame g){
 		}
 		printf("| %u\n", game_get_expected_nb_tents_row(g, i));
 	}
-	printf("   --------\n");
+	printf("   --------   \n");
 	printf("   ");
 	for (uint i = 0; i < 8; i++){
 		printf("%u", game_get_expected_nb_tents_col(g, i));
 	}
-
+	printf("   \n");
 }
 
 game game_default(void){
-	square square[] = {0, 0, 0, 0, 1, 1, 0, 0, 1 ,0 ,0, 0 ,0 ,0 ,0 ,1 ,0 ,0 ,0 ,0 ,1 ,0 ,0 ,0 , 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0};
+	square square[] = {0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0};
 	uint nb_tents_row[] = {3, 0, 4, 0, 4, 0, 1, 0};
 	uint nb_tents_col[] = {4, 0, 1, 2, 1, 1, 2, 1};
-
 	return game_new(square, nb_tents_row, nb_tents_col);
 }
 
@@ -79,5 +55,4 @@ game game_default_solution(void){
 	uint nb_tents_col[] = {4, 0, 1, 2, 1, 1, 2, 1};
 	return game_new(square, nb_tents_row, nb_tents_col);
 }
-
 
--- game_text.c
+++ game_text.c
@@ -4,6 +4,53 @@
 #include <stdlib.h>
 
 int main (void){
-	game_print(game_default());
+    game current_game = game_default();
+    char c;
+    uint i, j;
+    while(! game_is_over(current_game)){
+        game_print(current_game);
+        printf("Enter type of square, row number and column number:\n");
+        int ret1 = scanf("%c", &c);
+        if (c == 'h'){
+            printf("Press r to restart the game and q to quit\n");
+        }
+        else if (c == 'r'){
+            printf("You restarted the game\n");
+            game_restart(current_game);
+            game_print(current_game);
+            printf("Enter type of square, row number and column number:\n");
+        }
+        else if (c == 'q'){
+            printf("shame\n");
+            return EXIT_SUCCESS;
+        }
+        else if (ret1 == EOF){
+            printf("End of file\n");
+            exit(EXIT_SUCCESS);
+        }
+        int ret2 = scanf("%u %u", &i, &j);
+        if(ret1 == 1 && ret2 == 2 && (c == 't' || c == 'g' || c == 'e') && i < 8 && j < 8){
+            if (game_get_square(current_game, i, j) != TREE){
+                square s;
+                if (c == 't'){
+                    s = TENT;
+                }
+                if (c == 'g'){
+                    s = GRASS;
+                }
+                if (c == 'e'){
+                    s = EMPTY;
+                }
+                printf("You put %c in the (%u, %u) position\n", c, i, j);
+                game_play_move(current_game, i, j, s);
+            }
+        }
+        else if (ret2 == EOF){
+            printf("End of file\n");
+            exit(EXIT_SUCCESS);
+        }
+    }
+    game_print(current_game);
+    printf("congratulation\n");
     return EXIT_SUCCESS;
 }
