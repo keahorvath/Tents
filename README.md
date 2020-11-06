0eb782ccd9a6e87c05049a566b1f4142514e3a1c
Marvin Beites <marvin.beites@etu.u-bordeaux.fr>
2020-11-06 17:09:44 +0100

    Modifications tests marbeites et readme.md

--- README.md
+++ README.md
@@ -1,35 +1,37 @@
 # TEST
+
 ## **Alex Leymarie**
-    game_print 
-    game_default 
-    game_default_solution 
-    game_new 
-    game_new_empty 
+
+    game_print
+    game_default
+    game_default_solution
+    game_new
+    game_new_empty
     game_copy
 
-
 ## **Kea Horvath**
-    game_play_move 
-    game_check_move 
-    game_is_over 
-    game_fill_grass_row 
-    game_fill_grass_col 
+
+    game_play_move
+    game_check_move
+    game_is_over
+    game_fill_grass_row
+    game_fill_grass_col
     game_restart
 
+## **Marvin Beites**
 
-## **Marvine Marbeites**
-    game_get_expected_nb_tents_row 
-    game_get_expected_nb_tents_col 
-    game_get_expected_nb_tents_all 
-    game_get_current_nb_tents_row  
-    game_get_current_nb_tents_col  
+    game_get_expected_nb_tents_row
+    game_get_expected_nb_tents_col
+    game_get_expected_nb_tents_all
+    game_get_current_nb_tents_row
+    game_get_current_nb_tents_col
     game_get_current_nb_tents_all
 
-
 ## **Amira Mastouri**
-    game_equal game_delete 
-    game_set_square 
-    game_get_square 
-    game_set_expected_nb_tents_row 
-    game_set_expected_nb_tents_col
 
+    game_equal
+    game_delete
+    game_set_square
+    game_get_square
+    game_set_expected_nb_tents_row
+    game_set_expected_nb_tents_col
--- game_test_marbeites.c
+++ game_test_marbeites.c
@@ -34,12 +34,12 @@ bool test_get_expected_nb_tents_row(int k){
 
 bool test_get_expected_nb_tents_col(int k){
     if((k<0)|(k>7)){
-        fprintf(stderr, "Error : this column doesn't exist\n");
+        fprintf(stderr, "Error: this column doesn't exist\n");
         return false;
     }
     game g = game_default_solution();
     if(g==NULL){
-        fprintf(stderr, "Error : invalid game\n");
+        fprintf(stderr, "Error: invalid game\n");
         return false;
     }
     int tents=0;
@@ -82,12 +82,12 @@ bool test_get_expected_nb_tents_all(void){
 
 bool test_get_current_nb_tents_row(int k){
     if((k<0)|(k>7)){
-        fprintf(stderr, "Error : this row doesn't exist\n");
+        fprintf(stderr, "Error: this row doesn't exist\n");
         return false;
     }
     game g = game_default();
     if(g==NULL){
-        fprintf(stderr, "Error : invalid game\n");
+        fprintf(stderr, "Error: invalid game\n");
         return false;
     }
     int tents=0;
@@ -98,7 +98,7 @@ bool test_get_current_nb_tents_row(int k){
         }
     }
     if(tents!=game_get_current_nb_tents_row(g,k)){
-        fprintf(stderr, "Error : unexpected amount of tents\n");
+        fprintf(stderr, "Error: unexpected amount of tents\n");
         return false;
     }
     game_delete(g);
@@ -106,9 +106,13 @@ bool test_get_current_nb_tents_row(int k){
 };
 
 bool test_get_current_nb_tents_col(int k){
+    if((k<0)|(k>7)){
+        fprintf(stderr, "Error: this column doesn't exist\n");
+        return false;
+    }
     game g = game_default();
     if(g==NULL){
-        fprintf(stderr, "Error : invalid game\n");
+        fprintf(stderr, "Error: invalid game\n");
         return false;
     }
     int tents=0;
@@ -151,186 +155,6 @@ bool test_get_current_nb_tents_all(void){
 
 //Main routine
 int main(int argc, char *argv[]){
-    bool ok = false;
-    if(strcmp("get_expected_nb_tents_row", argv[1])==0){
-        ok = test_get_expected_nb_tents_row(0);
-    }
-    if(strcmp("get_expected_nb_tents_col", argv[1])==0){
-        ok = test_get_expected_nb_tents_col(0);
-    }
-    if(strcmp("get_expected_nb_tents_all", argv[1])==0){
-        ok = test_get_expected_nb_tents_all();
-    }
-    if(strcmp("get_current_nb_tents_row", argv[1])==0){
-        ok = test_get_current_nb_tents_row(0);
-    }
-    if(strcmp("get_current_nb_tents_col", argv[1])==0){
-        ok = test_get_current_nb_tents_col(0);
-    }
-    if(strcmp("get_current_nb_tents_all", argv[1])==0){
-        ok = test_get_current_nb_tents_all();
-    }
-    #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include "game.h"
-#include "game_aux.h"
-
-/*Tests*/
-
-bool test_get_expected_nb_tents_row(int k){
-    if((k<0)|(k>7)){
-        fprintf(stderr, "Error : this row doesn't exist\n");
-        return false;
-    }
-    game g = game_default_solution();
-    if(g==NULL){
-        fprintf(stderr, "Error : invalid game\n");
-        return false;
-    }
-    int tents=0;
-    for(int i=0; i<8; i++){
-        square a = game_get_square(g,k,i);
-        if(a==2){
-            tents++;
-        }
-    }
-    if(tents!=game_get_expected_nb_tents_row(g,k)){
-        fprintf(stderr, "Error : unexpected amount of tents\n");
-        return false;
-    }
-    game_delete(g);
-    return true;
-};
-
-bool test_get_expected_nb_tents_col(int k){
-    if((k<0)|(k>7)){
-        fprintf(stderr, "Error : this column doesn't exist\n");
-        return false;
-    }
-    game g = game_default_solution();
-    if(g==NULL){
-        fprintf(stderr, "Error : invalid game\n");
-        return false;
-    }
-    int tents=0;
-    for(int i=0; i<8; i++){
-        square a = game_get_square(g,i,k);
-        if(a==2){
-            tents++;
-        }
-    }
-    if(tents!=game_get_expected_nb_tents_col(g,k)){
-        fprintf(stderr, "Error : unexpected amount of tents\n");
-        return false;
-    }
-    game_delete(g);
-    return true;
-};
-
-bool test_get_expected_nb_tents_all(void){
-    game g = game_default_solution();
-    if(g==NULL){
-        fprintf(stderr, "Error : invalid game\n");
-        return false;
-    }
-    int tents=0;
-    for(int i=0; i<8; i++){
-        for(int j=0; j<8; j++){
-            square a=game_get_square(g,i,j);
-            if(a==2){
-                tents++;
-            }
-        }
-    }
-    if(tents!=game_get_expected_nb_tents_all(g)){
-        fprintf(stderr, "Error: unexpected amount of tents\n");
-        return false;
-    }
-    game_delete(g);
-    return true;
-};
-
-bool test_get_current_nb_tents_row(int k){
-    if((k<0)|(k>7)){
-        fprintf(stderr, "Error : this row doesn't exist\n");
-        return false;
-    }
-    game g = game_default();
-    if(g==NULL){
-        fprintf(stderr, "Error : invalid game\n");
-        return false;
-    }
-    int tents=0;
-    for(int i=0; i<8; i++){
-        square a = game_get_square(g,k,i);
-        if(a==2){
-            tents++;
-        }
-    }
-    if(tents!=game_get_current_nb_tents_row(g,k)){
-        fprintf(stderr, "Error : unexpected amount of tents\n");
-        return false;
-    }
-    game_delete(g);
-    return true;
-};
-
-bool test_get_current_nb_tents_col(int k){
-    game g = game_default();
-    if(g==NULL){
-        fprintf(stderr, "Error : invalid game\n");
-        return false;
-    }
-    int tents=0;
-    for(int i=0; i<8; i++){
-        square a=game_get_square(g,i,k);
-            if(a==2){
-                tents++;
-            }
-        }
-    if(tents!=game_get_current_nb_tents_col(g,k)){
-        fprintf(stderr, "Error: unexpected amount of tents\n");
-        return false;
-    }
-    game_delete(g);
-    return true;
-};
-
-bool test_get_current_nb_tents_all(void){
-    game g = game_default();
-    if(g==NULL){
-        fprintf(stderr, "Error : invalid game\n");
-        return false;
-    }
-    int tents=0;
-    for(int i=0; i<8; i++){
-        for(int j=0; j<8; j++){
-            square a=game_get_square(g,i,j);
-            if(a==2){
-                tents++;
-            }
-        }
-    }
-    if(tents!=game_get_current_nb_tents_all(g)){
-        fprintf(stderr, "Error: unexpected amount of tents\n");
-        return false;
-    }
-    game_delete(g);
-    return true;
-};
-
-//USAGE
-
-void usage(int argc, char *argv[]){
-    fprintf(stderr, "Usage: %s <testname> [<...>]\n", argv[0]);
-    exit(EXIT_FAILURE);
-}
-
-//Main routine
-int main(int argc, char *argv[]){
-    if(argc==1) usage(argc, argv);
     //start test
     fprintf(stderr, "=> Start test \"%s\"\n", argv[1]);
     bool ok = false;
@@ -352,20 +176,7 @@ int main(int argc, char *argv[]){
     if(strcmp("get_current_nb_tents_all", argv[1])==0){
         ok = test_get_current_nb_tents_all();
     }
-    else{
-        fprintf(stderr, "Error: test \"%s\" not found!\n", argv[1]);
-        exit(EXIT_FAILURE);
-    }
 
-    //print test result
-    if(ok){
-        fprintf(stderr, "Test \"%s\" finished: SUCCESS\n", argv[1]);
-        return EXIT_SUCCESS;
-    } else{
-        fprintf(stderr, "Test \"%s\" finished: FAILURE\n", argv[1]);
-        return EXIT_FAILURE;
-    }
-}
     //print test result
     if(ok){
         fprintf(stderr, "Test \"%s\" finished: SUCCESS\n", argv[1]);
