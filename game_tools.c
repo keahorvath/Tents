0edb83b1ea8cf6d2db74743b799547b6209ccc71
Mastouri <amira.mastouri@etu.u-bordeaux.fr>
2021-02-04 14:12:38 +0100

    ajout des deux fichiers tools et modification du CMake

---
+++ game_tools.c
@@ -0,0 +1,14 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "game.h"
+
+game game_load(char *filename){
+    game g = game_new_empty();
+    return g;
+}
+
+void game_save(cgame g, char *filename){
+    return;
+}
+
