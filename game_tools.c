be620a5ba8a0faf89d32e3648f1aff8687359471
Marvin Beites <marvin.beites@etu.u-bordeaux.fr>
2021-02-05 16:27:20 +0100

    Modifications save

--- game_tools.c
+++ game_tools.c
@@ -14,20 +14,8 @@ void game_save(cgame g, char *filename) {
   f = fopen(filename, "w");
   if (f != NULL) {
     fprintf(f, "%d %d ", game_nb_rows(g), game_nb_cols(g));
-    if (game_is_wrapping(g)) {
-      int wrapping = 1;
-      fprintf(f, "%d ", wrapping);
-    } else {
-      int wrapping = 0;
-      fprintf(f, "%d", wrapping);
-    }
-    if (game_is_diagadj(g)) {
-      int diagadj = 1;
-      fprintf(f, "%d\n", diagadj);
-    } else {
-      int diagadj = 0;
-      fprintf(f, "%d\n", diagadj);
-    }
+    fprintf(f, "%d ", game_is_wrapping(g));
+    fprintf(f, "%d\n", game_is_diagadj(g));
     for (int i = 0; i < game_nb_rows(g); i++) {
       fprintf(f, "%d ", game_get_expected_nb_tents_rows(g, i));
     }
