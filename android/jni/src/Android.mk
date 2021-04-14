9d58ccaa20f81b3674fb7255f9a5f406403ceb8d
Kea Horvath <kea.horvath@etu.u-bordeaux.fr>
2021-04-14 23:30:25 +0200

    tentative

--- android/jni/src/Android.mk
+++ android/jni/src/Android.mk
@@ -4,13 +4,13 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := main
 
-SDL_PATH := ../SDL
+SDL_PATH := ../SDL-2.0.5
 
 LOCAL_CFLAGS += -g -std=c99
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(SDL_PATH)/include
 
-YOUR_SRC_FILES= dlist.c game_aux.c game_solve.c graphic_mode.c game.c game_sdl.c game_tools.c queue.c
+YOUR_SRC_FILES= dlist.c game_sdl.c game_tools.c main.c game.c game_solve.c  graphic_mode.c  queue.c
 
 LOCAL_SRC_FILES := $(SDL_PATH)/src/main/android/SDL_android_main.c $(YOUR_SRC_FILES)
 
